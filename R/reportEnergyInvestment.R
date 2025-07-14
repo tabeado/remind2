02fc24941503e4913709961986b59ca1bd9955ea
tabeado <tabea.dorndorf@pik-potsdam.de>
2025-07-14 17:01:38 +0200

    add biochar to reportEnergyInvestment

--- R/reportEnergyInvestment.R
+++ R/reportEnergyInvestment.R
@@ -240,6 +240,12 @@ reportEnergyInvestment <- function(gdx, regionSubsetList = NULL,
   tmp <- mbind(tmp, setNames(inv_se(ie = pebio, oe = "sehe", pe2se, adjte, v_directteinv, v_adjustteinv), "Energy Investments|Heat|+|Biomass (billion US$2017/yr)"))
   tmp <- mbind(tmp, setNames(inv_se(ie = NULL, oe = NULL, c("tdhes"), adjte, v_directteinv, v_adjustteinv, te = all_te), "Energy Investments|Heat|+|Grid (billion US$2017/yr)"))
 
+  # Biochar
+  tmp <- mbind(tmp, setNames(inv_se(ie = "pebiolc", oe = "sebiochar", pe2se, adjte, v_directteinv, v_adjustteinv),  "Energy Investments|Biochar (billion US$2017/yr)"))
+  tmp <- mbind(tmp, setNames(inv_se(ie = "pebiolc", te = "biopyronly", oe = "sebiochar", pe2se, adjte, v_directteinv, v_adjustteinv), "Energy Investments|Biochar|+|no co-product (billion US$2017/yr)"))
+  tmp <- mbind(tmp, setNames(inv_se(ie = "pebiolc", te = c("biopyrhe","biopyrel", "biopyrchp"), oe = "sebiochar", pe2se, adjte, v_directteinv, v_adjustteinv), "Energy Investments|Biochar|+|heat or electricity (billion US$2017/yr)"))
+  tmp <- mbind(tmp, setNames(inv_se(ie = "pebiolc", te = "biopyrliq", oe = "sebiochar", pe2se, adjte, v_directteinv, v_adjustteinv), "Energy Investments|Biochar|+|Liquids (billion US$2017/yr)"))
+
   tmp <- mbind(tmp, setNames((tmp[, , "Energy Investments|Supply (billion US$2017/yr)"]
   - tmp[, , "Energy Investments|Electricity (billion US$2017/yr)"]
     - tmp[, , "Energy Investments|Hydrogen (billion US$2017/yr)"]
@@ -247,6 +253,7 @@ reportEnergyInvestment <- function(gdx, regionSubsetList = NULL,
     - tmp[, , "Energy Investments|Heat (billion US$2017/yr)"]
     - tmp[, , "Energy Investments|Gases (billion US$2017/yr)"]
     - tmp[, , "Energy Investments|Solids (billion US$2017/yr)"]
+    - tmp[, , "Energy Investments|Biochar (billion US$2017/yr)"]
     - tmp[, , "Energy Investments|CO2 Trans&Stor (billion US$2017/yr)"]),
   "Energy Investments|Other (billion US$2017/yr)"))
 
