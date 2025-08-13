55b7d615a61b467a4b07202c24dcf3986e1ae5e5
lecfab <lecfab+git@gmail.com>
2025-08-13 16:00:14 +0200

    light refactoring

--- R/reportPE.R
+++ R/reportPE.R
@@ -20,54 +20,44 @@
 #'
 #' @export
 #' @importFrom gdx readGDX
-#' @importFrom magclass mselect getYears getNames<- mbind setNames
+#' @importFrom magclass mselect getYears getNames<- mbind setNames matchDim
 
 reportPE <- function(gdx, regionSubsetList = NULL, t = c(seq(2005, 2060, 5), seq(2070, 2110, 10), 2130, 2150)) {
   ####### conversion factors ##########
   TWa_2_EJ <- 3600 * 24 * 365 / 1e6
   ####### read in needed data #########
   ## sets
-  teCCS    <- readGDX(gdx, "teCCS", format = "first_found") # technologies with carbon capture
-  teNoCCS  <- readGDX(gdx, "teNoCCS", format = "first_found") # technologies without CCS
-  entySe   <- readGDX(gdx, "entySe", format = "first_found") # secondary energy types
-  peFos    <- readGDX(gdx, "peFos", format = "first_found") # primary energy fossil fuels
-  peBio    <- readGDX(gdx, "peBio", format = "first_found") # biomass primary energy types
+  teCCS    <- readGDX(gdx, "teCCS") # technologies with carbon capture
+  teNoCCS  <- readGDX(gdx, "teNoCCS") # technologies without CCS
+  entySe   <- readGDX(gdx, "entySe") # secondary energy types
+  peFos    <- readGDX(gdx, "peFos") # primary energy fossil fuels
+  peBio    <- readGDX(gdx, "peBio") # biomass primary energy types
   pe2se    <- readGDX(gdx, "pe2se") # map primary energy carriers to secondary
-  pc2te    <- readGDX(gdx, "pc2te", format = "first_found") # prod couple: mapping for own consumption of technologies
+  pc2te    <- readGDX(gdx, "pc2te") # prod couple: mapping for own consumption and co-production of technologies
   pc2te    <- pc2te[(pc2te$all_enty1 %in% entySe) & (pc2te$all_enty2 %in% entySe), ] # ensure main and couple product are valid entySe
 
   seLiq    <- intersect(c("seliqfos", "seliqbio"), entySe)
   seGas    <- intersect(c("segafos", "segabio"), entySe)
   seSol    <- intersect(c("sesofos", "sesobio"), entySe)
 
-  ## parameter
-  pm_costsPEtradeMp <- readGDX(gdx, "pm_costsPEtradeMp", restore_zeros = FALSE)
-  
   ## variables
-  demPE  <- readGDX(gdx, name = "vm_demPe", field = "l", restore_zeros = FALSE, format = "first_found") * TWa_2_EJ
-  demPE  <- demPE[pe2se]
-  prodSE <- readGDX(gdx, name = "vm_prodSe", field = "l", restore_zeros = FALSE, format = "first_found") * TWa_2_EJ
-  prodSE <- mselect(prodSE, all_enty1 = entySe)
-  fuExtr <- readGDX(gdx, "vm_fuExtr", field = "l", format = "first_found") * TWa_2_EJ
-  Mport  <- readGDX(gdx, "vm_Mport", field = "l", format = "first_found") * TWa_2_EJ
-  Xport  <- readGDX(gdx, "vm_Xport", field = "l", format = "first_found") * TWa_2_EJ
+  demPE  <- readGDX(gdx, name = "vm_demPe", field = "l", restore_zeros = FALSE)
+  prodSE <- readGDX(gdx, name = "vm_prodSe", field = "l", restore_zeros = FALSE)
+  y <- Reduce(intersect, list(getYears(demPE), getYears(prodSE))) # calculate minimal temporal resolution
+  demPE  <- demPE[, y, pe2se] * TWa_2_EJ
+  prodSE <- prodSE[, y, entySe] * TWa_2_EJ
+  fuExtr <- readGDX(gdx, "vm_fuExtr", field = "l")[, y, ] * TWa_2_EJ
+  Mport  <- readGDX(gdx, "vm_Mport", field = "l")[, y, ] * TWa_2_EJ
+  Xport  <- readGDX(gdx, "vm_Xport", field = "l")[, y, ] * TWa_2_EJ
 
-  ####### calculate minimal temporal resolution #####
-  y <- Reduce(intersect, list(getYears(demPE), getYears(prodSE)))
-  demPE  <- demPE[, y, ]
-  prodSE <- prodSE[, y, ]
-  fuExtr <- fuExtr[, y, ]
-  Mport  <- Mport[, y, ]
-  Xport  <- Xport[, y, ]
+  ## parameters
+  prodCouple <- readGDX(gdx, "pm_prodCouple", restore_zeros = FALSE) # share of couple production
+  prodCouple <- magclass::matchDim(prodCouple, prodSE, dim = 1, fill = 0) # adjust regional dimension
+  prodCouple[prodCouple < 0] <- 0 # ignore negative values (own consumption of technologies)
 
-  #### adjust regional dimension of prodCouple (own consumption of technologies)
-  prodCouple_tmp <- readGDX(gdx, "pm_prodCouple", restore_zeros = FALSE, format = "first_found")
-  prodCouple_tmp[is.na(prodCouple_tmp)] <- 0
-  prodCouple <- new.magpie(getRegions(prodSE), getYears(prodCouple_tmp), magclass::getNames(prodCouple_tmp), fill = 0)
-  prodCouple[getRegions(prodCouple_tmp), , ] <- prodCouple_tmp
-  getSets(prodCouple) <- getSets(prodCouple_tmp)
-  prodCouple[prodCouple < 0] <- 0 # fix negative values to 0
-  
+  pm_costsPEtradeMp <- readGDX(gdx, "pm_costsPEtradeMp", restore_zeros = FALSE)
+
+
   ####### internal functions for reporting ###########
   get_demPE <- function(PEcarrier, SEcarrier = entySe, te = pe2se$all_te, name = NULL) {
     # Compute the PE of technologies that have SEcarrier as their main product
